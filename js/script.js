19ce2d4dbe3b8364f9a5bb38ea1b6664e5c8714d
jomakerforGod <74284597+joseph0305@users.noreply.github.com>
2022-02-11 20:01:24 +0900

    finshed js

--- js/script.js
+++ js/script.js
@@ -7,6 +7,8 @@ const continue_btn = info_box.querySelector(".buttons .restart");
 const quiz_box = document.querySelector(".quiz_box");
 const option_list = document.querySelector(".option_list");
 const timeCount = quiz_box.querySelector(".timer .timer_sec"); 
+const timeLine = quiz_box.querySelector("header .time_line");
+const timeOff = quiz_box.querySelector("header .time_text");
 
 
 
@@ -27,14 +29,44 @@ continue_btn.onclick = () => {
     showQuestions(0);
     queCounter(1);
     startTimer(timeValue);
+    startTimerLine(0);
 }
 
 let que_count = 0;
 let que_num = 1;
 let counter;
+let counterLine;
 let timeValue = 15;
+let widthValue = 0;
+let userScore = 0;
 
-const next_btn = quiz_box.querySelector(".next_btn")
+const next_btn = quiz_box.querySelector(".next_btn");
+const result_box = document.querySelector(".result_box");
+const restart_quiz = result_box.querySelector(".buttons .restart"); 
+const quit_quiz = result_box.querySelector(".buttons .quit"); 
+
+restart_quiz.onclick = () => {
+    quiz_box.classList.add("activeQuiz");
+    result_box.classList.remove("activeResult");
+    let que_count = 0;
+    let que_num = 1;
+    let timeValue = 15;
+    let widthValue = 0;
+    let userScore = 0;
+    showQuestions(que_count);
+    queCounter(que_num);
+    clearInterval(counter);
+    startTimer(timeValue);
+    clearInterval(counterLine);
+    startTimerLine(widthValue);
+    next_btn.style.display = "none";
+    timeOff.textContent = "Time Left";
+}
+
+
+quit_quiz.onclick = () => {
+    window.location.reload();
+}
 
 // If next Button Clicked
 next_btn.onclick = () => {
@@ -45,8 +77,15 @@ next_btn.onclick = () => {
         queCounter(que_num);
         clearInterval(counter);
         startTimer(timeValue);
+        clearInterval(counterLine);
+        startTimerLine(widthValue);
+        next_btn.style.display = "none";
+        timeOff.textContent = "Time Left";
     }else{
+        clearInterval(counter);
+        clearInterval(counterLine);
         console.log("Questions completed")
+        showResultBox();
     }
 }
 
@@ -72,10 +111,13 @@ let crossIcon = '<div class="icon cross"><i class="fas fa-times"></i></div>';
 
 
 function optionSelected(answer){
+    clearInterval(counter);
+    clearInterval(counterLine);
     let userAns = answer.textContent // 해당요소의 text
     let correctAns = questions[que_count].answer;
     let allOptions = option_list.children.length;
     if(userAns == correctAns){
+        userScore += 1;
         answer.classList.add("correct");
         console.log("OK");
         answer.insertAdjacentHTML("beforeend",tickIcon); //?????
@@ -97,6 +139,7 @@ function optionSelected(answer){
     for (let i = 0; i < allOptions; i++) {
         option_list.children[i].classList.add("disabled");
     }
+    next_btn.style.display = "block";
 }
 
 
@@ -108,15 +151,71 @@ function queCounter(index){
 
 }
 
+function showResultBox(){
+    info_box.classList.remove("activeInfo");
+    quiz_box.classList.remove("activeQuiz");
+    result_box.classList.add("activeResult");
+    const scoreText = result_box.querySelector(".score_text");
+    if(userScore > 3){
+        let scoreTag = '<span>and congrats, you got <p>&nbsp'+ userScore +'&nbsp</p> out of <p>&nbsp'+ questions.length+ '</p></span>';
+        scoreText.innerHTML = scoreTag;
+    }
+    else if(userScore > 1){
+        let scoreTag = '<span>and nice, you got only <p>&nbsp'+ userScore +'&nbsp</p> out of <p>&nbsp'+ questions.length+ '</p></span>';
+        scoreText.innerHTML = scoreTag;
+    }
+    else{
+        let scoreTag = '<span>and sorry, you got only <p>&nbsp'+ userScore +'&nbsp</p> out of <p>&nbsp'+ questions.length+ '</p></span>';
+        scoreText.innerHTML = scoreTag;
+    }
+}
+
 function startTimer(time){            // figure it out some day
     counter = setInterval(timer , 1000);
     function timer(){
         timeCount.textContent = time;
         time--;
+        if(time < 9){
+            let addZero = timeCount.textContent;
+            timeCount.textContent = "0" + addZero;
+        }
+        if(time < 0){
+            clearInterval(counter);
+            timeCount.textContent = "00";
+            timeOff.textContent = "Time Off";
+
+            let correctAns = questions[que_count].answer;
+            let allOptions = option_list.children.length;
+
+            // if did not choose the answer untill times up then automtically selected the correct answer
+            for (let i = 0; i < allOptions; i++) {
+                if(option_list.children[i].textContent == correctAns){
+                option_list.children[i].setAttribute("class", "option correct")
+                option_list.children[i].insertAdjacentHTML("beforeend",tickIcon);
+                }
+            }
+            // disabled all options
+            for (let i = 0; i < allOptions; i++) {
+                option_list.children[i].classList.add("disabled");
+            }
+            next_btn.style.display = "block";
+        }
     }
     // 1초 마다 특정함수를 실행시키고 싶을때
     // 예시: var timer = setInterval(function(){ 특정함수(); }, 1000)
 }   // 이를 중지하고 싶을때
     // clearInterval(timer)
 
-// 39:32
+
+
+function startTimerLine(time){            
+    counterLine = setInterval(timer , 29);
+    function timer(){
+        time += 1;
+        timeLine.style.width = time + "px"; 
+        if(time > 549){
+            clearInterval(counterLine);
+        }
+    }
+
+}
